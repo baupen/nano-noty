9f150d32ceafef81fd48193027462cf443ace221
Florian Moser <git@famoser.ch>
2026-06-09 09:00:59 +0200

    ref: Move lib outside for publishing

--- webpack.config.js
+++ webpack.config.js
@@ -25,7 +25,7 @@ module.exports = function (_, argv) {
       ...themeEntries
     },
     output: {
-      path: path.resolve(__dirname, 'demo/lib'),
+      path: path.resolve(__dirname, 'lib'),
       filename: '[name].js',
       library: {
         name: 'Noty',
